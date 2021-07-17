19a61e13b65f31d8970eaa2c11df81f347529338
aloisiot <loistc.carvalho@gmail.com>
2021-07-17 12:24:12 -0300

    add transiçao de cor de fundo do header durante evento scroll

--- jogos/js/background-top-bar.js
+++ jogos/js/background-top-bar.js
@@ -4,11 +4,9 @@ function backgroundTopBar()
 {
     const topBar = document.getElementById("top-bar");
 
-    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100)
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
     {
-        topBar.style.background = "#000000";
-        topBar.style.background = "-webkit-linear-gradient(to left, #434343, #000000)";
-        topBar.style.background = "linear-gradient(to left, #434343, #000000)";
+        topBar.style.backgroundColor = "rgba(0,0,0,0.7)";
 
     }
     else
