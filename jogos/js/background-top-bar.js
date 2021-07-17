f7d32f34f522dac0aedc5d59ab18d28d359f8e9b
HenriqueC4 <contatohenriquec@gmail.com>
2021-07-17 11:55:56 -0300

    Atualizando página

--- jogos/js/background-top-bar.js
+++ jogos/js/background-top-bar.js
@@ -1,12 +1,12 @@
-// window.onscroll = ()=>
+window.onscroll = ()=> backgroundTopBar();
 
 function backgroundTopBar()
 {
     const topBar = document.getElementById("top-bar");
 
-    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
+    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100)
     {
-        topBar.style.background = "#0000";
+        topBar.style.background = "#000000";
         topBar.style.background = "-webkit-linear-gradient(to left, #434343, #000000)";
         topBar.style.background = "linear-gradient(to left, #434343, #000000)";
 
