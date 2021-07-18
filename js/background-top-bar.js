42ebc1a9306e4286fa141deea5089d22d06cb8e3
aloisiot <loistc.carvalho@gmail.com>
2021-07-17 21:31:50 -0300

    inserçao de comentario relevante

--- js/background-top-bar.js
+++ js/background-top-bar.js
@@ -1,5 +1,7 @@
+// Monitora evento de rolagem da pagina
 window.onscroll = ()=> backgroundTopBar();
 
+// Altera propriedades de estilo para determinado elemento baseado em condições
 function backgroundTopBar()
 {
     const topBar = document.getElementById("top-bar");
