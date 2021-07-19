def6a39dcd511a61431606a66517e8a1054815a1
aloisiot <loistc.carvalho@gmail.com>
2021-07-19 01:30:53 -0300

    responsividade e menu hamburguer

--- js/menu-togle.js
+++ js/menu-togle.js
@@ -2,8 +2,6 @@
 
 // Busca elementos do html e armazena-os em variaveis
 const menu=document.getElementById('toggle-screen');
-const closeButton=document.getElementById('close');
-const body=document.getElementsByTagName('body');
 
 // Essa função, quando executada, propriedades de estilo de determinado elemento
 function displayMenuToggle(){
@@ -13,4 +11,44 @@ function displayMenuToggle(){
 // Essa função, quando executada, propriedades de estilo de determinado elemento
 function fechar(){
     menu.style.display = 'none';
+}
+
+// Essa função, quando executada, altera a propriedades display
+//  e redireciona para outra posição na pagina
+function fecharRedirecionarHome(){
+    menu.style.display = 'none';
+    window.location.href("#");
+}
+
+// Essa função, quando executada, altera a propriedades display
+//  e redireciona para outra posição na pagina
+function fecharRedirecionarJogos(){
+    menu.style.display = 'none';
+    window.location.href("#jogos");
+}
+
+// Essa função, quando executada, altera a propriedades display
+//  e redireciona para outra posição na pagina
+function fecharRedirecionarContatos(){
+    menu.style.display = 'none';
+    window.location.href("#contatos");
+}
+
+// Essa função, quando executada, altera a propriedades display
+//  e redireciona para outra posição na pagina
+function fecharRedirecionarAventura(){
+    menu.style.display = 'none';
+    window.location.href("#jogos-aventura");
+}
+
+// Essa função, quando executada, altera a propriedades display
+//  e redireciona para outra posição na pagina
+function fecharRedirecionarAcao(){
+    menu.style.display = 'none';
+    window.location.href("#jogos-acao");
+}
+
+function fecharRedirecionarLoin(){
+    menu.style.display = 'none';
+    window.location.href("../index.html");
 }
